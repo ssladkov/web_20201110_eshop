e02efd74c98d398993545d3c21276882dd2c9b98
ssladkov <me@ssladkov.ru>
2021-04-02 20:47:54 +0300

    Изменена страница вывода каталога

--- catalog.php
+++ catalog.php
@@ -1,17 +1,28 @@
 <!-- Это страница вывода каталога -->
 <?php
+    include($_SERVER['DOCUMENT_ROOT'].'/parts/header_conf.php');
+    $category_id = 1;
+    if(isset($_GET['id'])) {
+        $category_id = $_GET['id'];
+    }
+    $sql = "SELECT * FROM categories WHERE id = $category_id";
+    $category = get_db_one_record($link, $sql);
+    if($category == 404) {
+        header('HTTP/1.0 404 Not Found');
+        die;
+    }
+
     $template = [
-        'title' => 'Eshop: Каталог',
+        'title' => "Eshop: Каталог: {$category['name']}",
         'page_css' => 'catalog.css',
         'page_js' => ['catalog.js', 'product.js'],
     ];
-    include($_SERVER['DOCUMENT_ROOT'].'/parts/functions.php');
     include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
 ?>
 <div class="wrapper">
     <div class="catalog">
         <div class="catalog-header">
-            <div class="catalog-header-h1">Мужчинам</div>
+            <div class="catalog-header-h1"><?=$category['name'];?></div>
             <div class="catalog-header-desc">Всё товары</div>
         </div>
         <div class="catalog-products"></div>
