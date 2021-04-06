13a207875a1fff4c6b0051b0cc3f7c1e6ae0f823
ssladkov <me@ssladkov.ru>
2021-04-06 20:42:06 +0300

    Добавляет механику пагинации в каталоге

--- handlers/handlerCatalog.php
+++ handlers/handlerCatalog.php
@@ -1,12 +1,25 @@
 <?php
+/**
+ * @var $link string
+ */
 //Файл для получения данных о каталоге
 include($_SERVER['DOCUMENT_ROOT']. '/parts/header_conf.php');
+
 $category_id = $_GET['category_id'];
-$response = [
-    'products' => []
-];
+$per_page = 1;
+$current_page = !isset($_GET['page']) ? 1 : $_GET['page'];
+$from_record = ($current_page - 1) * $per_page;
 
 $sql_products = "SELECT * FROM products INNER JOIN product_category ON products.id = product_category.product_id WHERE product_category.category_id = {$category_id}";
-$response['products'] = get_db_result_assoc($link, $sql_products);
+//заменяет * на COUNT(*) в строчке $sql_products
+$sql_count_products = str_replace('*', 'COUNT(*)', $sql_products);
+$response = [
+    'products' => get_db_result_assoc($link, $sql_products, null, [$from_record, $per_page]),
+    'pagination' => [
+        'current_page' => $current_page,
+        'pages_count' => get_records_agregate_by($link, $sql_count_products),
+    ],
+];
+//$response['products'] = get_db_result_assoc($link, $sql_products);
 sleep(1);
 echo json_encode($response);
