aeada70621844ec64bcb44e7474db6c119bc5f3a
ssladkov <me@ssladkov.ru>
2021-04-06 22:01:18 +0300

    Добавляет функционал фильтра для каталога

--- handlers/handlerCatalog.php
+++ handlers/handlerCatalog.php
@@ -6,11 +6,24 @@
 include($_SERVER['DOCUMENT_ROOT']. '/parts/header_conf.php');
 
 $category_id = $_GET['category_id'];
-$per_page = 1;
+$per_page = 10;
+$filter_price_min = 0;
+$filter_price_max = 9999999;
+
+if(isset($_GET['filter_price'])) {
+    $filter_arr = explode(':', $_GET['filter_price']);
+    $filter_price_min = $filter_arr[0];
+    $filter_price_max = $filter_arr[1];
+    $filter_price_sql_where_and = " AND price >= '$filter_price_min 'AND price <= '$filter_price_max'";
+} else {
+    $filter_price_sql_where_and = null;
+}
+//todo: сделать фильтр для размеров
 $current_page = !isset($_GET['page']) ? 1 : $_GET['page'];
 $from_record = ($current_page - 1) * $per_page;
 
-$sql_products = "SELECT * FROM products INNER JOIN product_category ON products.id = product_category.product_id WHERE product_category.category_id = {$category_id}";
+
+$sql_products = "SELECT * FROM products INNER JOIN product_category ON products.id = product_category.product_id WHERE product_category.category_id = {$category_id}" . $filter_price_sql_where_and;
 //заменяет * на COUNT(*) в строчке $sql_products
 $sql_count_products = str_replace('*', 'COUNT(*)', $sql_products);
 $response = [
