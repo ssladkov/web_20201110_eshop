6fa2cd0ed3c03737ab6232822ed7229950df2b6a
kovil1402 <kovil666@gmail.com>
2021-04-09 14:23:20 +0300

    Работа над ошибками

--- parts/menu.php
+++ parts/menu.php
@@ -1,15 +1,15 @@
 <header>
-    <header-nav>
+    <nav>
         <img class="logo" src="../uploads/icons/logo.jpg">
         <a href="/catalog.php?id=1">Мужчинам</a>
         <a href="/catalog.php?id=2">Женщинам</a>
         <a href="/catalog.php?id=3">Аксессуары</a>
         <a href="">О нас</a>
-    </header-nav>
-    <header-cart>
+    </nav>
+    <section class="cart">
         <img src="../uploads/icons/account.png">
         <a>Войти</a>
         <img src="../uploads/icons/bascet.png">
         <a href="/cart.php">Корзина</a>
-    </header-cart>
+    </section>
 </header>
