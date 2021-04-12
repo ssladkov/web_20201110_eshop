7a69b6e1eecc24d69a0fa35476af8f9363cdaf2b
romanx241 <hohlovr@mail.ru>
2021-04-12 20:30:53 +0300

    Добавлены css, scss для форм авторизации и регистрации

--- auth.php
+++ auth.php
@@ -11,12 +11,21 @@ include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
 <div class="wrapper">
     <div class="registration">
         <form action="/handlers/handlerAuth.php?action=auth" method="POST">
-            <label for="login">Логин</label>
-            <input type="text" class="form-control" name="login" id="login"/>
-            <label for="password">Пароль</label>
-            <input type="password" class="form-control" name="password" id="password"/>
-            <input type="submit" value="Авторизоваться"/>
-        </form> 
+            <div class="form_field">
+                <h2>Авторизация</h2>
+                <div class="login">
+                    <label for="login" class="required">Логин:</label>
+                    <input type="text" class="form-control" name="login" id="login" required>
+                </div>
+                <div class="password">
+                    <label for="password" class="required">Пароль:</label>
+                    <input type="password" class="form-control" name="password" id="password" required>
+                </div>
+                <div class="submit">
+                    <input type="submit" value="Авторизоваться"/>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
 
--- register.php
+++ register.php
@@ -16,24 +16,44 @@ include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
 <div class="wrapper">
     <div class="registration">
         <form action="/handlers/handlerAuth.php?action=register" method="POST">
-            <label for="login">Выберите никнейм</label>
-            <input type="text" class="form-control" name="login" id="login"/>
-            <label for="password">Пароль</label>
-            <input type="password" class="form-control" name="password" id="password"/>
-            <label for="password_second">Повторите пароль</label>
-            <input type="password" class="form-control" name="password_second" id="password_second"/>
-            <label for="first_name">Имя</label>
-            <input type="text" class="form-control" name="first_name" id="first_name"/>
-            <label for="last_name">Фамилия</label>
-            <input type="text" class="form-control" name="last_name" id="last_name"/>
-            <label for="email">E-mail</label>
-            <input type="email" class="form-control" name="email" id="email"/>
-            <label for="phone">Телефон</label>
-            <input type="text" class="form-control" name="phone" id="phone"/>
-            <input type="submit" value="Создать пользователя"/>
-        </form> 
+            <div class="form_field">
+                <h2>Регистрация</h2>
+                <div class="login">
+                    <label for="login" class="required">Выберите никнейм:</label>
+                    <input type="text" class="form-control" name="login" id="login" required>
+                </div>
+                <div class="password">
+                    <label for="password" class="required">Пароль:</label>
+                    <input type="password" class="form-control" name="password" id="password" required>
+                    </div>
+                <div class="password_second">
+                    <label for="password_second" class="required">Повторите пароль:</label>
+                    <input type="password" class="form-control" name="password_second" id="password_second" required>
+                    </div>
+                <div class="first_name">
+                    <label for="first_name" class="required">Имя:</label>
+                    <input type="text" class="form-control" name="first_name" id="first_name" required>
+                </div>
+                <div class="last_name">
+                    <label for="last_name" class="required">Фамилия:</label>
+                    <input type="text" class="form-control" name="last_name" id="last_name" required>
+                </div>
+                <div class="email">
+                    <label for="email" class="required">Адрес электронной почты:</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+                <div class="phone">
+                    <label for="phone" class="required">Телефон:</label>
+                    <input type="text" class="form-control" name="phone" id="phone" required>
+                </div>
+                <div class="submit">
+                    <input type="submit" value="Создать пользователя"/>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
+
 
 <script>
     document.addEventListener('DOMContentLoaded', ()=> {
