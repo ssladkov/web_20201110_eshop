4dbbfbc42b4bf2858c0a913c2eeb20f37420f046
kovil1402 <kovil666@gmail.com>
2021-04-12 13:48:52 +0300

    добавил стили для главной страницы и футера

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,36 @@
-<?php foreach($template['page_js'] as $script) : ?>
-    <script src="/assets/js/<?=$script;?>"></script>
+<footer>
+    <div class="footer-element1">
+        <div>
+            <h4>Коллекции</h4>
+            <a>Женщинам</a><br>
+            <a>Мужчинам</a><br>
+            <a>Детям</a><br>
+            <a>Новинки</a>
+        </div>
+    </div>
+    <div class="footer-element2">
+        <div>
+            <h4>Магазин</h4>
+            <a>О нас</a><br>
+            <a>Доставка</a><br>
+            <a>Работай с нами</a><br>
+            <a>Контакты</a>
+        </div>
+    </div>
+    <div class="footer-element3">
+        <div>
+            <h4>Мы в социальных сетях</h4>
+            <a>Сайт разработан inordic.ru</a><br>
+            <a>Все права защищены</a>
+        </div>
+    </div>
+</footer>
+<!-- конец wrapper  -->
+</div>
+<!-- конец wrapper  -->
+<?php foreach ($template['page_js'] as $script) : ?>
+    <script src="/assets/js/<?= $script; ?>"></script>
 <?php endforeach; ?>
 </body>
+
 </html>
--- parts/menu.php
+++ parts/menu.php
@@ -1,14 +1,14 @@
 <header>
     <nav>
-        <img class="logo" src="../uploads/icons/logo.jpg">
-        <a href="/catalog.php?id=1">Мужчинам</a>
-        <a href="/catalog.php?id=2">Женщинам</a>
-        <a href="/catalog.php?id=3">Аксессуары</a>
-        <a href="">О нас</a>
+        <a href="/"><img class="logo" src="../uploads/icons/logo.jpg"></a>
+        <a class="menu-item" href="/catalog.php?id=1">Мужчинам</a>
+        <a class="menu-item" href="/catalog.php?id=2">Женщинам</a>
+        <a class="menu-item" href="/catalog.php?id=3">Аксессуары</a>
+        <a class="menu-item" href="">О нас</a>
     </nav>
     <section class="cart">
         <img src="../uploads/icons/account.png">
-        <a>Войти</a>
+        <a class="a-padding-right">Войти</a>
         <img src="../uploads/icons/bascet.png">
         <a href="/cart.php">Корзина</a>
     </section>
