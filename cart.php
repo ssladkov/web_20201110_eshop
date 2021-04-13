4f0e70cfed8c7ed33c89a4796dddd51651fe9204
kovil1402 <kovil666@gmail.com>
2021-04-13 14:54:38 +0300

    work in progress

--- cart.php
+++ cart.php
@@ -2,15 +2,28 @@
 include($_SERVER['DOCUMENT_ROOT'] . '/parts/header_conf.php');
 
 $template = [
-    'title' => "Eshop: Корзина}",
+    'title' => "Eshop: Корзина",
     'page_css' => 'cart.css',
     'page_js' => ['cart.js']
 ];
 include($_SERVER['DOCUMENT_ROOT'] . '/parts/header.php');
 ?>
 
-<div class="cart">
-
+<div class="wrapper">
+    <div class="headline">
+        <div>
+            <h1>Ваша Корзина</h1>
+            <p>Товары резервируются на ограниченное время</p>
+        </div>
+    </div>
+    <div class="table-column-names">
+        <p>Фото</p>
+        <p>Наименование</p>
+        <p>Размер</p>
+        <p>Количество</p>
+        <p>Стоимость</p>
+        <p>Удалить</p>
+    </div>
 </div>
 
 <script>
