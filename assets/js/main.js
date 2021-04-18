3c19fc3d5885aaca65fc75e7f1a5751eea1fec02
iipospelov <i.i.pospelov@mail.ru>
2021-04-18 17:02:42 +0300

    доработан e-mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,28 @@
-console.log(window);
 window.addEventListener("load", () => {
-    console.log(window);
     document.querySelector('.lds-backdrop').classList.add('disabled');
     let subscribeEl = document.querySelector('form');
     let emailInputEl = document.querySelector('.email_input input[name=email]');
     let errorTextEl = document.querySelector('.error-text');
-    console.log(subscribeEl.value);
     subscribeEl.addEventListener('submit', (e)=> {
-        console.log(subscribeEl);
         e.preventDefault();
         if(emailInputEl.value == ''){
-            console.log(emailInputEl);
             errorTextEl.style.display = 'block';
-            return false;
         }
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', '/parts/handlerSubscribe.php?email=' + emailInputEl.value);
+        xhr.open('GET', '/handlers/handlerSubscribe.php?email=' + emailInputEl.value);
         xhr.send();
         xhr.addEventListener('load', ()=> {
             if(xhr.responseText == 'ok') {
-                subscribeEl.submit();
-                console.log(xhr.responseText);
+                errorTextEl.style.display = 'block';
+                errorTextEl.innerHTML = 'Вы успешно подписались на рассылку!';
+                errorTextEl.style.color = 'green';
+                emailInputEl.style.display = 'none';
+                subscribeEl.querySelector('input[type=submit]').style.display = 'none';
             } else if(xhr.responseText == 'not_uniq') {
-                console.log(xhr.responseText);
+                errorTextEl.style.display = 'block';
                 errorTextEl.innerHTML = 'Такой e-mail уже существует!';
             }
-        })
-        console.log(xhr);
+        });
+        return false;
     })
 });
--- data/db/20210409_last_web_20201011__eshop.sql
+++ data/db/20210409_last_web_20201011__eshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 16 2021 г., 23:11
+-- Время создания: Апр 18 2021 г., 13:28
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -43,6 +43,41 @@ INSERT INTO `categories` (`id`, `name`, `create_time`, `update_time`) VALUES
 (1, 'Мужское', '2021-04-02 20:08:44', '2021-04-02 20:08:44'),
 (2, 'Женское', '2021-04-02 20:08:44', '2021-04-02 20:08:44'),
 (3, 'Аксессуары', '2021-04-02 20:08:59', '2021-04-02 20:08:59');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `delivery_method` varchar(150) NOT NULL,
+  `payment_method` varchar(150) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `update_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'new'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_product`
+--
+
+CREATE TABLE `order_product` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `amount` smallint(6) NOT NULL,
+  `size` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -100,6 +135,64 @@ INSERT INTO `product_category` (`id`, `product_id`, `category_id`, `create_time`
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `product_size`
+--
+
+CREATE TABLE `product_size` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `size_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `product_size`
+--
+
+INSERT INTO `product_size` (`id`, `product_id`, `size_id`) VALUES
+(8, 1, 8),
+(7, 1, 9),
+(9, 1, 10),
+(10, 1, 11),
+(1, 2, 2),
+(2, 2, 3),
+(3, 2, 4),
+(4, 2, 5),
+(11, 3, 7),
+(12, 3, 8),
+(6, 4, 3),
+(5, 4, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sizes`
+--
+
+CREATE TABLE `sizes` (
+  `id` int(11) NOT NULL,
+  `label` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `sizes`
+--
+
+INSERT INTO `sizes` (`id`, `label`) VALUES
+(1, '39'),
+(2, '40'),
+(3, '41'),
+(4, '42'),
+(5, '43'),
+(6, '44'),
+(7, 'S'),
+(8, 'M'),
+(9, 'L'),
+(10, 'XL'),
+(11, 'XXL');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `subscribers`
 --
 
@@ -108,14 +201,30 @@ CREATE TABLE `subscribers` (
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Дамп данных таблицы `subscribers`
+-- Структура таблицы `users`
 --
 
-INSERT INTO `subscribers` (`id`, `email`) VALUES
-(1, 'i.i.pospelov@mail.ru'),
-(2, ''),
-(3, 'ff@jj');
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `create_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `hash`, `first_name`, `last_name`, `email`, `phone`, `is_admin`, `create_time`) VALUES
+(1, 'admin', '25d55ad283aa400af464c76d713c07ad', 'Админ', 'Главный', 'sladkov.sergey@yandex.ru', '+79098525252', 0, '2021-04-09 21:38:20');
 
 --
 -- Индексы сохранённых таблиц
@@ -126,6 +235,20 @@ INSERT INTO `subscribers` (`id`, `email`) VALUES
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order_product`
+--
+ALTER TABLE `order_product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Индексы таблицы `products`
@@ -142,10 +265,31 @@ ALTER TABLE `product_category`
   ADD KEY `product_id` (`product_id`);
 
 --
+-- Индексы таблицы `product_size`
+--
+ALTER TABLE `product_size`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_product_size` (`product_id`,`size_id`),
+  ADD KEY `size_id` (`size_id`);
+
+--
+-- Индексы таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `subscribers`
 --
 ALTER TABLE `subscribers`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_users_login` (`login`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -156,6 +300,18 @@ ALTER TABLE `subscribers`
 --
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `order_product`
+--
+ALTER TABLE `order_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `products`
@@ -170,14 +326,39 @@ ALTER TABLE `product_category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT для таблицы `product_size`
+--
+ALTER TABLE `product_size`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT для таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT для таблицы `subscribers`
 --
 ALTER TABLE `subscribers`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `order_product`
+--
+ALTER TABLE `order_product`
+  ADD CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_product_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Ограничения внешнего ключа таблицы `product_category`
@@ -185,6 +366,13 @@ ALTER TABLE `subscribers`
 ALTER TABLE `product_category`
   ADD CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `product_category_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `product_size`
+--
+ALTER TABLE `product_size`
+  ADD CONSTRAINT `product_size_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `product_size_ibfk_2` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
