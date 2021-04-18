178c680d4e92ce4f0c6dd716eaf0b7e1164a16cc
iipospelov <i.i.pospelov@mail.ru>
2021-04-18 17:02:42 +0300

    доработан e-mail

--- handlers/handlerSubscribe.php
+++ handlers/handlerSubscribe.php
@@ -1,12 +1,13 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'].'/parts/header_conf.php');
+if(isset($_GET['email'])){
+    include($_SERVER['DOCUMENT_ROOT'].'/parts/header_conf.php');
 
-$sql = "SELECT email FROM subscribers WHERE email = '{$_GET['email']}'";
-$result_email = get_db_one_record($link, $sql);
-if($result_email == 404) {
-    $result = insert_db_row($link, 'subscribers', ['email'], $_GET);
-    echo 'ok';
-} else {
-    echo 'not_uniq';
+    $sql = "SELECT email FROM subscribers WHERE email = '{$_GET['email']}'";
+    $result_email = get_db_one_record($link, $sql);
+    if($result_email == 404) {
+        $result = insert_db_row($link, 'subscribers', ['email'], $_GET);
+        echo 'ok';
+    } else {
+        echo 'not_uniq';
+    }
 }
-echo $_GET['email'];
